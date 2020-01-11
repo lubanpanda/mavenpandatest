36f682f6108570097559a2da582104e32ab84d34
panda <84305510@qq.com>
2020-01-11 20:09:04 +0800

    maven更新

--- src/others/com/panda/readExcel.java
+++ src/others/com/panda/readExcel.java
@@ -6,32 +6,31 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 
 import java.io.File;
 
-import java.io.FileOutputStream;
-import java.io.OutputStream;
-
 /**
  * @author panda   84305510@qq.com
  * @date 2019-06-29 18:40
  */
 public class readExcel {
     public static void main(String[] args) {
-        String fileDir = "/Users/panda/Desktop/hello.xls";
+        String fileDir = "/Users/panda/Desktop/Oracle_jmeter/多表对比输入文件.xlsx";
         File file = new File(fileDir);
         HSSFWorkbook workbook = null;
         if (file.exists()) {    //文件存在
             //创建workbook
             try {
                 workbook = new HSSFWorkbook();
-
-                HSSFSheet sheet = workbook.createSheet();
+                HSSFSheet sheet = workbook.getSheetAt(0);
                 //加入Worksheet（不加入sheet时生成的xls文件打开时会报错)
                 // HSSFSheet sheet = workbook.getSheetAt(0);
-                HSSFRow row = sheet.createRow(0);
-                row.createCell(0).setCellValue("nishuo");
-                OutputStream out = new FileOutputStream(new File(fileDir));
-                workbook.write(out);
-                out.close();
-                workbook.close();
+                // sheet=Oraclebook.getSheetAt(1);//到时候这里换上变量
+                System.out.println(sheet);
+                HSSFRow row = sheet.getRow(0);
+                int zerorow = row.getLastCellNum();
+                for (int i = 1; i < zerorow; i++) {
+                    String databasename = row.getCell(i).getStringCellValue();
+                    String[] strArr = databasename.split("\\-");
+                    System.out.println(strArr);
+                }
             } catch (Exception e) {
                 e.printStackTrace();
             }
